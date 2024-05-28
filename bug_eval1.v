82d62dfe1faf78c3e2507670dfa9b98ed2455f04
ptar124 <party48235@ymail.com>
2024-05-28 14:08:01 +0100

    start parse

---
+++ bug_eval1.v
@@ -0,0 +1,36 @@
+`include "syn_yosys.v"
+
+module bug_eval (result);
+
+    wire y1, y2;
+    wire [2:0] in_shiftandmult;
+    output reg [4:0] result;
+
+    top_1 eval_top_1 (.y(y1), .w(in_shiftandmult));
+
+    assign in_shiftandmult = 3'b100;
+
+    reg [3:0] a_num1;
+    reg [3:0] a_num2;
+    reg [3:0] b_num1;
+    reg [3:0] b_num2;
+    reg [4:0] a_sum;
+    reg [4:0] b_sum;
+
+    always @* begin
+        if (y1 == 1) begin
+            //GOOD BRANCH
+            a_num1 = 4'b0010;
+            a_num2 = 4'b0001;
+            a_sum = a_num1 + a_num2;
+            result = a_sum;
+        end
+        else begin
+            //BAD BRANCH           
+            b_num1 = 4'b1000;
+            b_num2 = 4'b0100;
+            b_sum = b_num1 + b_num2;
+            result = b_sum;
+        end
+    end
+endmodule
---
+++ goodbranch.v
@@ -0,0 +1,7 @@
+module goodbranch (num1, num2, sum);
+    input wire [3:0] num1;
+    input wire [3:0] num2;
+    output wire [4:0] sum;
+
+    assign sum = num1 + num2;
+endmodule
